00576406a38a4e00b3e97b90c1c18b27e7b1a2fe
Anson VanDoren <anson.vandoren@gmail.com>
2019-11-17 17:59:26 -0800

    initial commit

---
+++ main.go
@@ -0,0 +1,5 @@
+package main
+
+func main() {
+	p("ChitChat", version(), "started at", config.Address)
+}
---
+++ utils.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"text/template"
+)
+
+type Configuration struct {
+	Address      string
+	ReadTimeout  int64
+	WriteTimeout int64
+	Static       string
+}
+
+var config Configuration
+var logger *log.Logger
+
+// convenience function to print to stdout
+func p(a ...interface{}) {
+	fmt.Println(a)
+}
+
+func init() {
+	loadConfig()
+	file, err := os.OpenFile("chitchat.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalln("Failed to open log file", err)
+	}
+	logger = log.New(file, "INFO", log.Ldate|log.Ltime|log.Lshortfile)
+}
+
+func loadConfig() {
+	file, err := os.Open("config.json")
+	if err != nil {
+		log.Fatalln("Cannot open config file", err)
+	}
+	decoder := json.NewDecoder(file)
+	config = Configuration{}
+	err = decoder.Decode(&config)
+	if err != nil {
+		log.Fatalln("Cannot get configuration from file", err)
+	}
+}
+
+func generateHTML(w http.ResponseWriter, data interface{}, filenames ...string) {
+	var files []string
+	for _, file := range filenames {
+		files = append(files, fmt.Sprintf("templates/%s.html", file))
+	}
+
+	templates := template.Must(template.ParseFiles(files...))
+	templates.ExecuteTemplate(w, "layout", data)
+}
+
+func version() string {
+	return "0.1"
+}
